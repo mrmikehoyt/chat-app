5bb389835f3e3ce9ca4e87af5fd4cec760f34173
Miito <dinhlol69@gmail.com>
2020-07-16 19:34:15 -0400

    Add basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
 
 export default function App() {
   return (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
