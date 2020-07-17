c7f6ba9583737efbdf3a0bf76d9dc5dd8b2bd51d
Miito <dinhlol69@gmail.com>
2020-07-17 03:50:07 -0400

    Add Note Page

---
+++ src/pages/Notes/index.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/no-array-index-key */
+import React from 'react';
+import {
+  Typography,
+} from '@material-ui/core';
+
+function NotesPage() {
+  return (
+    <div>
+      <Typography>
+        Note Page
+      </Typography>
+    </div>
+  );
+}
+
+export default NotesPage;
