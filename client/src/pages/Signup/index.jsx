d15b9157bcb825df387e2fae6aef264c48d89eca
Miito <dinhlol69@gmail.com>
2020-07-22 14:34:48 -0400

    Implement authentication into client site

--- client/src/pages/Signup/index.jsx
+++ client/src/pages/Signup/index.jsx
@@ -1,5 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-shadow */
+import React, { useState, useEffect } from 'react';
 import {
   Avatar,
   Button,
@@ -8,13 +10,16 @@ import {
   Grid,
   Typography,
   Container,
-  Box,
+  Link,
+  FormGroup,
   makeStyles,
 } from '@material-ui/core';
+import { Alert, AlertTitle } from '@material-ui/lab';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import { Link as rLink } from 'react-router-dom';
-// import FormControlLabel from '@material-ui/core/FormControlLabel';
-// import Checkbox from '@material-ui/core/Checkbox';
+import { connect } from 'react-redux';
+import { register } from '../../actions/authActions';
+import { clearErrors } from '../../actions/errorActions';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -34,10 +39,44 @@ const useStyles = makeStyles((theme) => ({
   button: {
     margin: theme.spacing(3, 0, 2),
   },
+  alert: {
+    marginTop: theme.spacing(2),
+    width: '100%',
+  },
 }));
 
-export default function SignUp() {
+function SignUp({
+  isAuthenticated,
+  error,
+  register,
+  clearErrors,
+}) {
   const classes = useStyles();
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [alert, setAlert] = useState(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const user = {
+      name,
+      email,
+      password,
+    };
+
+    register(user);
+  };
+
+  useEffect(() => {
+    // Check for register error
+    if (error.id === 'REGISTER_FAIL') {
+      setAlert(error.message.error);
+    } else {
+      setAlert(null);
+    }
+  }, [error]);
 
   return (
     <Container component="main" maxWidth="xs">
@@ -49,52 +88,69 @@ export default function SignUp() {
         <Typography component="h1" variant="h5">
           Sign Up
         </Typography>
-        <form className={classes.form} noValidate>
+        {alert ? <Alert className={classes.alert} severity="error">{alert}</Alert> : null}
+        <form className={classes.form} noValidate onSubmit={handleSubmit}>
           <TextField
-            variant="filled"
+            value={name}
+            variant="outlined"
             margin="normal"
-            required
+            fullWidth
+            id="name"
+            label="Name"
+            name="name"
+            autoComplete="name"
+            autoFocus
+            onChange={(e) => setName(e.target.value)}
+          />
+          <TextField
+            value={email}
+            variant="outlined"
+            margin="normal"
             fullWidth
             id="email"
             label="Email Address"
             name="email"
             autoComplete="email"
             autoFocus
+            onChange={(e) => setEmail(e.target.value)}
           />
           <TextField
-            variant="filled"
+            value={password}
+            variant="outlined"
             margin="normal"
-            required
             fullWidth
             name="password"
             label="Password"
             type="password"
             id="password"
             autoComplete="current-password"
+            onChange={(e) => setPassword(e.target.value)}
           />
-          <Grid container>
-            <Box display="flex" justifyContent="space-between" flexGrow={1}>
-              <Button
-                component={rLink}
-                to="/login"
-                color="primary"
-                variant="contained"
-                className={classes.button}
-              >
-                Login
-              </Button>
-              <Button
-                type="submit"
-                variant="contained"
-                color="primary"
-                className={classes.button}
-              >
-                Sign Up
-              </Button>
-            </Box>
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            fullWidth
+            className={classes.button}
+          >
+            Sign Up
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link href="#" variant="body2" component={rLink} to="/login">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
           </Grid>
         </form>
       </div>
     </Container>
   );
 }
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+  error: state.error,
+});
+
+export default connect(mapStateToProps, { register, clearErrors })(SignUp);
