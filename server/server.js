3ef45f9aa373ce02d7be30bcf242659b028f3945
Miito <dinhlol69@gmail.com>
2020-07-22 15:01:26 -0400

    Add missing import

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const routes = require('./routes');
 
