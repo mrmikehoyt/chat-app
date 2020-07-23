c67a97d6ab7fe97d5e4d7314f60a423e4eaf8afd
Miito <dinhlol69@gmail.com>
2020-07-23 05:46:36 -0400

    Implement note reducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducers';
 import errorReducer from './errorReducers';
+import noteReducer from './noteReducers';
 
 export default combineReducers({
   auth: authReducer,
   error: errorReducer,
+  item: noteReducer,
 });
